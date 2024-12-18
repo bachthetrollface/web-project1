d2c7b2136f56d50587b483ebb1bc9e4fa2828e17
bachthetrollface <bachthetrollface@gmail.com>
2024-12-18 15:15:08 +0700

    test database setup

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField("Question", max_length=200)
+    date_posted = models.DateTimeField("Date posted")
