bc1d5b37097e3fee6c3c86dbab4eca1588e2851f
bachthetrollface <bachthetrollface@gmail.com>
2024-12-20 13:20:08 +0700

    URL paths reference

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -4,6 +4,10 @@ from .models import Question, Choice
 
 # Create your views here.
 
+# render returns a HttpResponse already
+# raise Http404 exception in case objects are not found
+# shortcuts: get_object_or_404 and get_list_or_404
+
 def index(request):
     # latest_question_list = Question.objects.order_by("date_posted")[:5]
     # output = " || ".join([q.question_text for q in latest_question_list])
@@ -11,7 +15,7 @@ def index(request):
     context = {
         "latest_question_list": Question.objects.order_by("date_posted")[:5]
     }
-    return HttpResponse(render(request, "polls/index.html", context))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     # question = "Question %s: %s" % (question_id, Question.objects.get(pk=question_id).question_text)
@@ -21,4 +25,4 @@ def detail(request, question_id):
         "question": Question.objects.get(pk=question_id),
         "choice_list": Choice.objects.filter(question=Question.objects.get(pk=question_id))
     }
-    return HttpResponse(render(request, "polls/detail.html", context))
+    return render(request, "polls/detail.html", context)
