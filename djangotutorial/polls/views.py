529107030d88f61a38a37aef8a7c34df4d0ac499
bachthetrollface <bachthetrollface@gmail.com>
2024-12-20 13:05:53 +0700

    try templates using html, need completion to form user-friendly UI

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    # latest_question_list = Question.objects.order_by("date_posted")[:5]
+    # output = " || ".join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    context = {
+        "latest_question_list": Question.objects.order_by("date_posted")[:5]
+    }
+    return HttpResponse(render(request, "polls/index.html", context))
 
 def detail(request, question_id):
-    return HttpResponse("Question %s" % question_id)
+    # question = "Question %s: %s" % (question_id, Question.objects.get(pk=question_id).question_text)
+    # choices = ' || '.join([choice.choice_text for choice in Choice.objects.filter(question=Question.objects.get(pk=question_id))])
+    # return HttpResponse(question + ' || ' + choices)
+    context = {
+        "question": Question.objects.get(pk=question_id),
+        "choice_list": Choice.objects.filter(question=Question.objects.get(pk=question_id))
+    }
+    return HttpResponse(render(request, "polls/detail.html", context))
