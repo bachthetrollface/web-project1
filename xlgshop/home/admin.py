4cf1a0c17cf3ad5a773536590d2c06ef00b49dbb
bachthetrollface <bachthetrollface@gmail.com>
2024-12-22 00:48:14 +0700

    rework database, 2nd try

--- xlgshop/home/admin.py
+++ xlgshop/home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Type, AvailableSizeAndColor
 
 # Register your models here.
 admin.site.register(Item)
+admin.site.register(Type)
+admin.site.register(AvailableSizeAndColor)
--- xlgshop/home/models.py
+++ xlgshop/home/models.py
@@ -2,52 +2,45 @@ from django.db import models
 
 # Create your models here.
 
-# class Item(models.Model):
-#     item_id = models.CharField("ID", max_length=5, primary_key=True)
-#     item_name = models.CharField("Name", max_length=30)
-#     item_type = models.CharField("Type", max_length=30)
-#     size = models.CharField("Size", max_length=5)
-#     add_date = models.DateTimeField("Time Added")
-
 """
 1 relation for each type of item,
 1 size relation for each item type above,
 items store availability of sizes: has_s, has_m, has_l, has_xl,...,
 colors?: different ids for different colors
 relations for storing available colors
+
+table for saving purchases
 """
 
-class TShirt(models.Model):
-    pass
+class Type(models.Model):
+    name = models.CharField("Name", max_length=20, primary_key=True)
+    
+    def __str__(self):
+        return self.name
 
-class Trousers(models.Model):
-    pass
 
-class Hoodie(models.Model):
-    pass
+class Item(models.Model):
+    id = models.CharField("ID", max_length=5, primary_key=True)
+    name = models.CharField("Name", max_length=30, blank=False)
+    type = models.ForeignKey(Type, on_delete=models.RESTRICT, blank=False)
+    add_date = models.DateTimeField("Time Added")
+    
+    def __str__(self):
+        return f"{self.id} - {self.name}"
 
-class Jeans(models.Model):
-    pass
 
-class Sweater(models.Model):
-    pass
-
-class AvailableColorsTShirt(models.Model):
-    item_type = models.ForeignKey(TShirt, on_delete=models.CASCADE, primary_key=True)
-    color = models.CharField("Color", primary_key=True)
-
-class AvailableColorsTrousers(models.Model):
-    item_type = models.ForeignKey(Trousers, on_delete=models.CASCADE, primary_key=True)
-    color = models.CharField("Color", primary_key=True)
-
-class AvailableColorsHoodie(models.Model):
-    item_type = models.ForeignKey(Hoodie, on_delete=models.CASCADE, primary_key=True)
-    color = models.CharField("Color", primary_key=True)
-
-class AvailableColorsJeans(models.Model):
-    item_type = models.ForeignKey(Jeans, on_delete=models.CASCADE, primary_key=True)
-    color = models.CharField("Color", primary_key=True)
-
-class AvailableColorsSweater(models.Model):
-    item_type = models.ForeignKey(Sweater, on_delete=models.CASCADE, primary_key=True)
-    color = models.CharField("Color", primary_key=True)
+class AvailableSizeAndColor(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    size = models.CharField("Size", max_length=3)
+    color = models.CharField("Color", max_length=20)
+    price = models.FloatField("Price", blank=False)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['item', 'size', 'color'], name='primary_key'
+            )
+        ]
+    
+    def __str__(self):
+        return f"{self.item.__str__()} - {self.size} - {self.color}"
